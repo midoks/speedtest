8aab7bfcf2abe7a7eb7f6cc930ca2b917bf2fdc6
midoks <midoks@163.com>
2019-10-03 16:29:43 +0800

    Update io.sh

--- scripts/io.sh
+++ scripts/io.sh
@@ -3,7 +3,7 @@
 time dd if=/dev/zero of=/dev/null bs=4k count=256000
 
 
-time dd if=/dev/zero of=/dev/null bs=4096k count=25600
+time dd if=/dev/zero of=/dev/null bs=4096k count=2560
 
 
 time dd if=/dev/zero of=/dev/null bs=40960k count=2560
